9358419882be8c4d00a26b11f4b0ced5c326ba9b
Alamgir-K <k.alamgir2002@gmail.com>
2022-07-20 18:16:57 -0400

    solved merge intervals

---
+++ merge_intervals/merge_intervals.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+
+class Interval:
+  def __init__(self, start, end):
+    self.start = start
+    self.end = end
+
+  def print_interval(self):
+    print("[" + str(self.start) + ", " + str(self.end) + "]", end='')
+
+
+def merge_interval(arr: list) -> list:
+    """
+    Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
+
+    Time Complexity: O(nlogn)
+    Space Complexity: O(n)
+    """
+    arr.sort(key=lambda x:x.start)
+    merged = []
+    start = arr[0].start
+    end = arr[0].end
+
+    for i in range(1, len(arr)):
+        interval = arr[i]
+
+        if interval.start <= end:
+            end = max(end, interval.end)
+        else:
+            merged.append(Interval(start, end))
+            start = interval.start
+            end = interval.end
+
+    merged.append(Interval(start, end))
+    return merged
+    
+
+
+def test_merge_interval():
+    print("Merged intervals: ", end='')
+    for i in merge_interval([Interval(1, 4), Interval(2, 5), Interval(7, 9)]):
+        i.print_interval()
+    print()
+
+    print("Merged intervals: ", end='')
+    for i in merge_interval([Interval(6, 7), Interval(2, 4), Interval(5, 9)]):
+        i.print_interval()
+    print()
+
+    print("Merged intervals: ", end='')
+    for i in merge_interval([Interval(1, 4), Interval(2, 6), Interval(3, 5)]):
+        i.print_interval()
+    print()
+
+
+if __name__ == "__main__":
+    test_merge_interval()
