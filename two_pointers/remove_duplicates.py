6c1e6b19b222c16f24f19be08890019aec143bc2
Alamgir-K <k.alamgir2002@gmail.com>
2022-07-20 00:01:06 -0400

    solved remove_element_duplicate

--- two_pointers/remove_duplicates.py
+++ two_pointers/remove_duplicates.py
@@ -25,5 +25,33 @@ def test_two_sum():
     assert two_sum(arr2) == 2
 
 
+def remove_element_duplicate(arr: list, k: int) -> int:
+    """
+    Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place and return the new length of the array.
+
+    Time Complexity: O(n) where n = len(arr) 
+    Space Complexity: O(1)
+    """
+    next = 0
+
+    for i in range(0, len(arr)):
+        if arr[i] != k:
+            arr[next] = arr[i]
+            next += 1  
+
+    return next
+
+    
+def test_remove_element_duplicate():
+    arr1 = [3, 2, 3, 6, 3, 10, 9, 3]
+    k1 = 3
+    assert remove_element_duplicate(arr1, k1) == 4
+
+    arr2 = [2, 11, 2, 2, 1]
+    k2 = 2
+    assert remove_element_duplicate(arr2, k2) == 2
+
+
 if __name__ == "__main__":
     test_two_sum()
+    test_remove_element_duplicate
