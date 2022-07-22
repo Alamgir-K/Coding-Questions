66898fb4ee6080ee426be02426413cb776a1b47c
Alamgir-K <k.alamgir2002@gmail.com>
2022-07-22 02:26:04 -0400

    solved max depth

--- bfs/minimum_depth.py
+++ bfs/minimum_depth.py
@@ -39,17 +39,51 @@ def minimum_depth(root: TreeNode) -> list:
     return depth 
 
 
+def maximum_depth(root: TreeNode):
+    """
+    Find the maximum depth of a binary tree. 
+
+    Time Complexity: O(n) where n = len(arr)
+    Space Complexity: O(n)
+    """
+    if root is None:
+        return -1
+
+    queue = deque()
+    queue.append(root)
+    depth = 0
+
+    while queue:
+        depth += 1
+        level_size = len(queue)
+
+        for _ in range(0, level_size):
+            current = queue.popleft()
+
+            if current.left:
+                queue.append(current.left)
+
+            if current.right:
+                queue.append(current.right)
+        
+    return depth
+
+
+
+
+
 def test_minimum_depth():
-  
     root = TreeNode(12)
     root.left = TreeNode(7)
     root.right = TreeNode(1)
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
     print("Tree Minimum Depth: " + str(minimum_depth(root)))
+    print("Tree Maximum Depth: " + str(maximum_depth(root)))
     root.left.left = TreeNode(9)
     root.right.left.left = TreeNode(11)
     print("Tree Minimum Depth: " + str(minimum_depth(root)))
+    print("Tree Maximum Depth: " + str(maximum_depth(root)))
 
 
 if __name__ == "__main__":
